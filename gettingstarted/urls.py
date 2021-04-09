8589c7d5b65bbc8b9736eb849c31e61bae8d6e10
Youshaext <yousharunescape@yahoo.com>
2021-04-09 12:15:23 +0500

    changed settings,urls to remove hello app

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
-
 from khana.admin import admin_site
 
 # To add a new path, first import the app:
@@ -17,8 +15,6 @@ from khana.admin import admin_site
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path('khana/', include('khana.urls')),
     path('myadmin/', admin_site.urls),
