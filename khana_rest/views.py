f3b5bd0d0752d9aacf9eb773a03b78b5fd5502fe
Yousha Arshad <yousha@Apples-MacBook-Pro.local>
2021-05-27 12:50:21 +0500

    update readme

--- khana_rest/views.py
+++ khana_rest/views.py
@@ -4,16 +4,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from khana.models import FoodItem
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 # Create your views here.
 
 # List all food items
+@api_view(['GET', 'POST'])
 def food_item_list(request):
     if request.method == 'GET':
         food_items = FoodItem.objects.all()
         serializer = FoodItemSerializer(food_items, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        #return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         pass
