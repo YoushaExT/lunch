66eb3c5a9381a8e02a4f435fcfe7869fa9825694
Yousha Arshad <yousha@Apples-MacBook-Pro.local>
2021-05-28 10:09:37 +0500

    create class based api for food_items

--- khana_rest/urls.py
+++ khana_rest/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 # from django.urls.resolvers import URLPattern
-from .views import food_item_list
+from .views import FoodItemAPIView, FoodItemDetails #, food_item_list
 
 urlpatterns = [
-    path('food_items/', food_item_list),
+    #path('food_items/', food_item_list),
+    path('food_items/', FoodItemAPIView.as_view()),
+    path('food_items/<int:pk>/', FoodItemDetails.as_view()),
+
 ]
--- khana_rest/views.py
+++ khana_rest/views.py
@@ -6,19 +6,68 @@ from rest_framework.parsers import JSONParser
 from khana.models import FoodItem
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 
+#from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-# List all food items
-@api_view(['GET', 'POST'])
-def food_item_list(request):
-    if request.method == 'GET':
+class FoodItemAPIView(APIView):
+
+    def get(self, request):
         food_items = FoodItem.objects.all()
         serializer = FoodItemSerializer(food_items, many=True)
         #return JsonResponse(serializer.data, safe=False)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        pass
-        # todo
+    def post(self, request):
+        serializer = FoodItemSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FoodItemDetails(APIView):
+
+    def get_object(self, pk):
+        try:
+            return FoodItem.objects.get(pk=pk)
+        
+        except FoodItem.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        item = self.get_object(pk)
+        serializer = FoodItemSerializer(item)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        item = self.get_object(pk)
+        serializer = FoodItemSerializer(item, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        item = self.get_object(pk)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+# # List all food items
+# #@login_required
+# @api_view(['GET', 'POST'])
+# def food_item_list(request):
+#     if request.method == 'GET':
+#         food_items = FoodItem.objects.all()
+#         serializer = FoodItemSerializer(food_items, many=True)
+#         #return JsonResponse(serializer.data, safe=False)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         #serializer = FoodItemSerializer(data=request.data)
+#         pass
+#         # todo
